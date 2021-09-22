b34deffd6abac7c684cdb8edfdb2dd3184777a63
Xiang Ying <xying871@gmail.com>
2021-09-22 18:33:15 -0400

    working on text widgets

--- app/src/main/java/algonquin/cst2335/XiangYing/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/XiangYing/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.XiangYing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView topView= findViewById(R.id.textview);
+        String oldText = topView.getText().toString();
+        topView.setText("Java put this here");
+
+        EditText bottom = findViewById(R.id.bottomtext);
+
+        Button btn = findViewById(R.id.button);
+        btn.setText("The view was previously "+ oldText);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                topView.setText("Edit text has " + bottom.getText())
+            }
+
+                               }
+
+        )
+                }
+        );
+       ;
     }
 }
